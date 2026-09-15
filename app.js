011198cd5671d2978093d9948decc7ce5cad893c
donnellbrown845-blip <donnellbrown845@gmail.com>
2026-09-14 22:18:33 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const productName = "RAM"
+const category = "NVME";
+let price = 70.00;
+let inStock = true;
+console.log("productName:", productName)
+console.log("category:", category)
+console.log("price:", price)
+console.log("inStock:", inStock)
